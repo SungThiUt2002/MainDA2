5af194e16050a9540730f8c1b58cc7e646d995bb
SungThiUt2002 <sungthiut2002@gmail.com>
2025-08-05 22:37:09 +0700

    5/8

--- shipping-service/src/main/java/com/stu/shipping_service/repository/ShippingStatusHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stu.shipping_service.repository;
-
-import com.stu.shipping_service.entity.ShippingStatusHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShippingStatusHistoryRepository extends JpaRepository<ShippingStatusHistory, Long> {
-} 
