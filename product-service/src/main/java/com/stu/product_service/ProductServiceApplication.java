7223783de9bfebd404809750ebdfa060b3026f72
SungThiUt2002 <sungthiut2002@gmail.com>
2025-08-14 23:52:23 +0700

    full config server

--- product-service/src/main/java/com/stu/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/stu/product_service/ProductServiceApplication.java
@@ -3,6 +3,7 @@ package com.stu.product_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @SpringBootApplication
 @EnableFeignClients
 @EnableKafka
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
